1a1069da6fb25bcf8f7e76aca059fa5a1653610c
ramkishun02 <ramkishun02@gmail.com>
2026-08-25 12:02:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -789,7 +789,7 @@ async function startServer() {
     process.exit(1);
   }
 }
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
