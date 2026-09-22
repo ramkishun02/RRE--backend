527d495a0d873ec8fd7603693e80c430fff318d9
ramkishun02 <ramkishun02@gmail.com>
2026-09-22 08:57:22 +0530

    Fix dotenv config line in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 cd /root/RRE--backend
-sed -i '1i require("dotenv").config();' server.js
+sed -i '1i require("dotenv").config()';
 const path = require("path");
 const crypto = require("crypto");
 const express = require("express");
